8a4d195272552cf2a6a8ed89f790ac01982c8f30
minda-luke <luke@theminda.com>
2023-12-21 13:58:38 +0900

    테스트용

---
+++ src/CustomCheckBox.tsx
@@ -0,0 +1,10 @@
+import { Checkbox } from '@mui/material'
+import React from 'react'
+
+export default function CustomCheckBox() {
+  return (
+    <Checkbox sx={{
+        color: 'secondary.main',
+    }}/>
+  )
+}
